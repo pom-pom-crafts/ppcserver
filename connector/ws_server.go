a70e568b17eaf58ed158b0c58977cfdec43c6a2a
lrui <lrchljw@gmail.com>
2022-04-17 20:14:42 +0800

    Add a simple WSServer accepts WebSocket connection and echo message back to the client.

--- connector/ws_server.go
+++ connector/ws_server.go
@@ -1,4 +1,67 @@
 package connector
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func StartWSServer(url string /* TODO, options ...Option */) (*WSServer, error) {
+	var upgrader *websocket.Upgrader
+	// TODO, one can pass customized upgrader from options
+	upgrader = &websocket.Upgrader{}
+
+	wsServer := &WSServer{
+		upgrader,
+	}
+	wsServer.Start()
+
+	return wsServer, nil
+}
+
 type WSServer struct {
+	upgrader *websocket.Upgrader
+}
+
+func (s *WSServer) Start() {
+	defer s.Shutdown()
+
+	http.Handle("/", s)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("http.ListenAndServe() fail", err)
+	}
+}
+
+func (s *WSServer) Shutdown() {
+	// TODO, graceful shutdown logic
+	log.Println("WSServer.Shutdown() to be implemented")
+}
+
+func (s *WSServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// TODO, handle websocket upgrade
+	conn, err := s.upgrader.Upgrade(w, r, nil)
+
+	// Log then return when Upgrade failed.
+	if err != nil {
+		log.Println("WSServer.upgrader.Upgrade() fail", err)
+		return
+	}
+
+	// TODO, wrap read and write in Client.
+	for {
+		msgType, msgInBytes, err := conn.ReadMessage()
+
+		if err != nil {
+			log.Println("conn.ReadMessage() fail", err)
+			break
+		}
+
+		log.Printf("recv: %s", msgInBytes)
+
+		err = conn.WriteMessage(msgType, msgInBytes)
+		if err != nil {
+			log.Println("conn.WriteMessage() fail", err)
+			break
+		}
+	}
 }
