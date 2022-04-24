e1a5fbfb001e732eb397c8761b0f98bf5e50a2a2
lrui <lrchljw@gmail.com>
2022-04-24 15:06:19 +0800

    Add four readyState (New, Authorized, Offline, Closed) to Client.

--- connector/client.go
+++ connector/client.go
@@ -1,16 +1,29 @@
 package connector
 
+const (
+	// ClientReadyStateNew is the readyState when the connection has established,
+	// and the server is waiting for auth or reconnect message from the peer.
+	ClientReadyStateNew clientReadyState = iota
+	ClientReadyStateAuthorized
+	ClientReadyStateOffline
+	ClientReadyStateClosed
+)
+
 type (
+	clientReadyState int
+
 	// Client represents a client connection to any server of the connector.
 	Client struct {
-		transport Transport
+		transport  Transport
+		readyState clientReadyState
 	}
 )
 
 // NewClient creates a new Client.
 func NewClient(transport Transport) *Client {
 	client := &Client{
-		transport: transport,
+		transport:  transport,
+		readyState: ClientReadyStateNew,
 	}
 
 	return client
@@ -23,6 +36,9 @@ func (c *Client) Write(data []byte) error {
 	return nil
 }
 
-func (c *Client) Close() {
+func (c *Client) Close() error {
 
+	// TODO, notify that client is closed.
+
+	return nil
 }
