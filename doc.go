f297081e068b57623429efccc2c6a6e4a95f7e8a
lrui <lrchljw@gmail.com>
2022-05-01 17:15:52 +0800

    Add doc.go.

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package ppcserver is a Go multiplayer game server framework featured real-time WebSocket communication,
+// Stateful Room, and distributed scaling via NATS.
+// The package is under development, intending to release the first available version in 2022.
+package ppcserver
